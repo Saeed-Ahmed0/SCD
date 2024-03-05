586fe906509e38448142014d1571abb80eddaebb
abdulwahab89 <abdulwahablaghari6@gmail.com>
2024-03-05 17:18:43 +0500

    more changes were made in app.js

--- app.js
+++ app.js
@@ -39,3 +39,14 @@ document.addEventListener('click', function(event) {
       closeNavbarMenu();
     }
 });
+$(document).ready(function(){
+    $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    });
+  });
